863a5b1211c11dbfb22da70d04a2fb23621fdb33
Pon Huang <carolyn722@gmail.com>
2022-07-12 19:31:53 +0800

    javascript

---
+++ index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const form = document.querySelector("form");
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const errorMessage = document.querySelector(".error-message");
+const errorIcon = document.querySelector(".error-icon");
+
+const input = [firstName, lastName, email, password];
+
+// prevent type one input then rest of the input has invalid class
+let isValidationOn = false;
+
+const resetElement = (element) => {
+  element.classList.remove("invalid");
+  errorIcon.classList.add("hidden");
+  errorMessage.classList.add("hidden");
+};
+
+const invalidateElement = (element) => {
+  element.classList.add("invalid");
+  errorIcon.classList.remove("hidden");
+  errorMessage.classList.remove("hidden");
+};
+
+const validateInputs = () => {
+  if (!isValidationOn) return;
+  input.forEach(resetElement);
+
+  if (!firstName.value) {
+    invalidateElement(firstName);
+  }
+  if (!lastName.value) {
+    invalidateElement(lastName);
+  }
+  if (!email.value.match(regexEmail)) {
+    invalidateElement(email);
+  }
+  if (!password.value) {
+    invalidateElement(password);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  isValidationOn = true;
+  validateInputs();
+});
+
+// by using forEach then we don't need to reiterate the function by ourself
+input.forEach((input) => {
+  input.addEventListener("input", () => {
+    validateInputs();
+  });
+});
+
+/*
+// for example, when user types in the input it run the validateInputs and remove inavlid class
+firstName.addEventListener("input", () => {
+  validateInputs();
+});
+*/
